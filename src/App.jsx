a82d1d4938d52e072db33d4d4554a178fbea9d14
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-08-21 13:10:50 +0530

    Added the logic for representation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 export default function App() {
+
+  const file = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  const kaspersky = [[1, 2], [7, 9]]
+
+  const isPresent = (index) => {
+    const status = kaspersky.every(obj => {
+      if ((obj[0] <= index) && index <= obj[1]) {
+        return false
+      }
+      else {
+        return true
+      }
+    })
+
+    return !status
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-3 flex justify-center items-center gap-3">
+      <div>Kaspersky</div>
+      <div className="w-full bg-slate-400 h-10 border-2 flex">
+        {
+          file.map((singleFile) => {
+            return <div key={singleFile} className={`h-full w-full ${isPresent(singleFile) ? "bg-red-400" : "bg-green-400"} border p-2 text-center`}>{singleFile}</div>
+          })
+        }
+      </div>
+    </div>
   )
 }
